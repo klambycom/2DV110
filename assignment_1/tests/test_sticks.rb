47b606c4c2436d3a7fd70d9da5a59435ece48f1d
klambycom <christian@klamby.com>
2014-11-13 11:42:53 +0100

    RED Constructor

--- assignment_1/tests/test_sticks.rb
+++ assignment_1/tests/test_sticks.rb
@@ -2,7 +2,7 @@ require './lib/sticks.rb'
 require 'test/unit'
 
 class TestSticks < Test::Unit::TestCase
-  def test_sample
-    assert_equal(4, 2 + 2)
+  def test_constructor
+    Sticks.new
   end
 end
