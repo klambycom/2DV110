b43de28a126333508b8e6ba1345b7e8a754ae3c1
klambycom <christian@klamby.com>
2014-11-13 12:11:10 +0100

    RED nr_of_sticks

--- assignment_1/tests/test_sticks.rb
+++ assignment_1/tests/test_sticks.rb
@@ -5,4 +5,9 @@ class TestSticks < Test::Unit::TestCase
   def test_constructor
     Sticks.new 15
   end
+
+  def test_nr_of_sticks
+    sticks = Sticks.new 15
+    assert_equal sticks.nr_of_sticks, 15
+  end
 end
