b5fa58b9c4c28835f39564b4f2b93f98ccdbb43f
klambycom <christian@klamby.com>
2014-11-13 12:50:34 +0100

    REFACTOR setup

--- assignment_1/tests/test_sticks.rb
+++ assignment_1/tests/test_sticks.rb
@@ -2,29 +2,29 @@ require './lib/sticks.rb'
 require 'test/unit'
 
 class TestSticks < Test::Unit::TestCase
+  def setup
+    @sticks = Sticks.new 15
+  end
+
   def test_constructor
     Sticks.new 15
   end
 
   def test_nr_of_sticks
-    sticks = Sticks.new 15
-    assert_equal sticks.nr_of_sticks, 15
+    assert_equal @sticks.nr_of_sticks, 15
   end
 
   def test_to_s
-    sticks = Sticks.new 15
-    assert_equal sticks.to_s, 'IIIIIIIIIIIIIII'
+    assert_equal @sticks.to_s, 'IIIIIIIIIIIIIII'
   end
 
   def test_draw_1
-    sticks = Sticks.new 15
-    sticks.draw_1
-    assert_equal sticks.nr_of_sticks, 14
+    @sticks.draw_1
+    assert_equal @sticks.nr_of_sticks, 14
   end
 
   def test_draw_2
-    sticks = Sticks.new 15
-    sticks.draw_2
-    assert_equal sticks.nr_of_sticks, 13
+    @sticks.draw_2
+    assert_equal @sticks.nr_of_sticks, 13
   end
 end
