2af59ecdb2b7fb930e0a4403eb9a260892f2f353
klambycom <christian@klamby.com>
2014-11-22 00:32:26 +0100

    RED computer (3 sticks)

--- assignment_1/tests/test_sticks.rb
+++ assignment_1/tests/test_sticks.rb
@@ -186,6 +186,15 @@ class TestSticks < Test::Unit::TestCase
     assert_equal 1, @sticks.nr_of_sticks
   end
 
+  def test_computer_3
+    @sticks.draw_3
+    @sticks.draw_3
+    @sticks.draw_3
+    @sticks.draw_3
+    @sticks.computer
+    assert_equal 1, @sticks.nr_of_sticks
+  end
+
   private
 
   def assert_sticks n, message = nil
