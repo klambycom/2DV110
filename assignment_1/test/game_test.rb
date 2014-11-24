81b18dbf23293c263f1329d625eece2ce45bc486
klambycom <christian@klamby.com>
2014-11-24 22:43:47 +0100

    RED computer winner

--- assignment_1/test/game_test.rb
+++ assignment_1/test/game_test.rb
@@ -10,18 +10,21 @@ class TestGame < MiniTest::Test
   def test_draw_1
     @sticks.expects(:draw_1).once
     @sticks.expects(:computer).once
+    @sticks.expects(:game_over?).returns(false)
     @game.draw 1
   end
 
   def test_draw_2
     @sticks.expects(:draw_2).once
     @sticks.expects(:computer).once
+    @sticks.expects(:game_over?).returns(false)
     @game.draw 2
   end
 
   def test_draw_3
     @sticks.expects(:draw_3).once
     @sticks.expects(:computer).once
+    @sticks.expects(:game_over?).returns(false)
     @game.draw 3
   end
 
@@ -29,5 +32,13 @@ class TestGame < MiniTest::Test
     assert_raises RuntimeError do
       @game.draw :not_a_number
     end
+  end
+
+  def test_draw_computer_is_winner
+    sticks = mock()
+    sticks.expects(:draw_1).once
+    sticks.expects(:game_over?).returns(true)
+    game = Game.new sticks
+    assert_equal :computer, game.draw(1)
   end
 end
