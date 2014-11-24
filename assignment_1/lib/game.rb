c796b172276906724555b6cbbdbe7b0c09906e9a
klambycom <christian@klamby.com>
2014-11-24 22:47:45 +0100

    GREEN computer winner

--- assignment_1/lib/game.rb
+++ assignment_1/lib/game.rb
@@ -4,6 +4,7 @@ class Game
   end
 
   def draw n
+    # Players draw
     case n
     when 1 then @sticks.draw_1
     when 2 then @sticks.draw_2
@@ -11,6 +12,10 @@ class Game
     else raise "Invalid argument"
     end
 
+    # Computer is winner if player had last draw
+    return :computer if @sticks.game_over?
+
+    # Computers draw
     @sticks.computer
   end
 end
